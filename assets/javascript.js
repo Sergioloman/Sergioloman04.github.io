a0d742c897c30ed4384234b5356ab7e1bd2a6122
Sergio Torres <sergio.lo.torres@gmail.com>
2021-03-08 12:49:32 -0600

    start quiz function completed

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,11 +1,16 @@
+//variables
 var timer = 15;
-var currentQuestionIndex =0;
-
+var currentQuestionIndex  =0;
+var score = 0;
+var quizScreen = document.querySelector('#quiz-screen');
 var quizTimerUpdate = document.querySelector('#timer');
 var startQuizButton = document.querySelector('#start-button');
-var revealQuestions = document.querySelector('#quiz-screen');
-var score = 0;
+var quizTimerScreen = document.querySelector('#timer-screen');
+var startScreen = document.querySelector('#start-screen');
+var endScreen = document.querySelector('#end-screen');
+var finalScore = document.querySelector('#score');
 
+//arrays
 var quizQuestions = [{
     Title: "How can you define a variable in a way its value stays the same?",
     Options: ["var", "let", "const", "if"],
@@ -22,23 +27,33 @@ var quizQuestions = [{
     Answer: "===",
 
 }];
-
+//event listeners
 startQuizButton.addEventListener('click',startQuiz);
-function startQuiz(){
-    revealQuiz.removeAttribute('class');
 
+//functions
+function startQuiz(){
+    startScreen.setAttribute('class','hide');
+    quizScreen.removeAttribute('class','hide');
+    quizTimerScreen.removeAttribute('class','hide');
+    countdownTimer();
+    getQuestions();
 };
 
 function countdownTimer() {
     var decreaseTime = setInterval(function () {
         if (timer > 0) {
             timer--;
-            quizTimerUpdate.textContent = 'you have ' + timer + ' seconds left';
+            quizTimerUpdate.textContent = timer;
         } else {
             quizTimerUpdate.textContent = 'Oops! Your time is up!';
             clearInterval(decreaseTime);
         };
-    }, 1000);
-    
+    }, 1000);  
 };
+
+function getQuestions(){
+
+};
+
+
 
